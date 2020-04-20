7b3ab899c47b1095d41f593a5fd1a05b98f79346
Cameron Cardow <ccardow@gmail.com>
2020-04-20 13:32:32 -0700

    added a clear button that shows only if the users.length > 0

--- src/components/users/Search.js
+++ src/components/users/Search.js
@@ -8,6 +8,8 @@ class Search extends Component {
 
   static propTypes = {
     searchUsers: PropTypes.func.isRequired,
+    clearUsers: PropTypes.func.isRequired,
+    showClear: PropTypes.bool.isRequired,
   };
 
   onSubmit = (e) => {
@@ -21,6 +23,7 @@ class Search extends Component {
   };
 
   render() {
+    const { showClear, clearUsers } = this.props;
     return (
       <div>
         <form onSubmit={this.onSubmit} className="form">
@@ -34,9 +37,14 @@ class Search extends Component {
           <input
             type="submit"
             value="Search"
-            className="btn btn-dark btn=block"
+            className="btn btn-dark btn-block"
           />
         </form>
+        {showClear && (
+          <button className="btn btn-light btn-block" onClick={clearUsers}>
+            Clear
+          </button>
+        )}
       </div>
     );
   }
